987ca680f94ff385f94afeccc28ca0c9d4d7e71b
Swagat Pattnaik <swagatpattnaik@gmail.com>
2020-12-28 13:06:04 -0500

    added coments to DemoController

--- src/main/java/com/boot/poc/controller/DemoController.java
+++ src/main/java/com/boot/poc/controller/DemoController.java
@@ -7,15 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+    // This is a simple GET request
     @GetMapping(path = "/demos")
     public String demoWorld(){
         return "Hello welcome to my Demo";
     }
-
+    // This is a simple GET request
     @GetMapping(path = "/demo-bean")
     public DemoWorldBean demoWorldBean(){
         return new DemoWorldBean("Hello welcome to my DemoBean");
     }
+    // Example for path variables
     @GetMapping(path = "/demo-bean/path-variables/{name}")
     public DemoWorldBean demoPathVariable(@PathVariable String name){
         return new DemoWorldBean(String.format("Path variable demo. Path variable passed:%s",name));
