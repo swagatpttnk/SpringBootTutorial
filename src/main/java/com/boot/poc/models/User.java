347c4e47df663e4d32df97bae612cfc15479eb4e
Swagat Pattnaik <swagatpattnaik@gmail.com>
2021-01-13 13:24:58 -0500

    added the Swagger annotation 2

--- src/main/java/com/boot/poc/models/User.java
+++ src/main/java/com/boot/poc/models/User.java
@@ -1,16 +1,21 @@
 package com.boot.poc.models;
 
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
-
+@ApiModel(description = "User is entity used for representing an Employee")
 public class User {
     private Integer id;
 
+    @ApiModelProperty(notes = "name of the user")
     @Size(min = 2,max = 10,message = "name should have atleast 2 and max 10 char")
     private String name;
 
+    @ApiModelProperty(notes = "Dat Of Birth of the user,should be in past")
     @Past
     private Date birthDate;
 
